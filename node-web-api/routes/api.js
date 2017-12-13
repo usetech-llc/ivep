a62f74d78ae8d372c9cf3ae9d2721441b7e9b7ee
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2017-12-13 15:23:37 +0300

    https://mc.usetech.ru/redmine/issues/27969 api

--- node-web-api/routes/api.js
+++ node-web-api/routes/api.js
@@ -1,11 +1,19 @@
 const helpers = require('./../helpers/helperFunctions');
 const transactions = require('./../controller/transaction.controller');
+const investors = require('./../controller/investor.controller');
 
 const routesAPI = function(app) {
-//articles api routes
+  // transactions api routes
   app.get('/api/transactions', helpers.isAuthenticated, transactions.get);
   app.get('/api/transactions/:transactionId', helpers.isAuthenticated, transactions.getOne);
   app.post('/api/transactions', helpers.isAuthenticated, transactions.insertOne);
-  app.put('/api/transactions', helpers.isAuthenticated, transactions.updateOne);
+  app.put('/api/transactions/:transactionId', helpers.isAuthenticated, transactions.updateOne);
   app.delete('/api/transactions/:transactionId', helpers.isAuthenticated, transactions.delete);
+
+  // investors api routes
+  app.get('/api/investors', helpers.isAuthenticated, investors.get);
+  app.get('/api/investors/:investorId', helpers.isAuthenticated, investors.getOne);
+  app.post('/api/investors', helpers.isAuthenticated, investors.insertOne);
+  app.put('/api/investors/:investorId', helpers.isAuthenticated, investors.updateOne);
+  app.delete('/api/investors/:investorId', helpers.isAuthenticated, investors.delete);
 };
