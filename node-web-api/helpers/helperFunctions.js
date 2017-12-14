ea4d5cd9a5dfd90bfbc8e5ae8ad3516c33309a46
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2017-12-14 15:44:27 +0300

    https://mc.usetech.ru/redmine/issues/27969 helper

--- node-web-api/helpers/helperFunctions.js
+++ node-web-api/helpers/helperFunctions.js
@@ -1,5 +1,6 @@
 const helpers = {};
 const Transactions = require('./../model/transaction.model');
+const Investors = require('./../model/investor.model');
 
 helpers.isAuthenticated = function(req, res, next){
   next();
@@ -7,14 +8,24 @@ helpers.isAuthenticated = function(req, res, next){
 
 //Function to populate data in DB if DB is empty.
 helpers.populateDb = function(){
-  var promise = Transactions.get();
-  promise.then(function(data){
+  const transactions = Transactions.get();
+  transactions.then(function(data){
     if(data.length){
       console.log('Transactions table already populated.');
     }
     else{
       console.log('Populating Transactions table.');
       Transactions.seed();
+    }
+  });
+  const investors = Investors.get();
+  investors.then(function(data){
+    if(data.length){
+      console.log('Investors table already populated.');
+    }
+    else{
+      console.log('Populating Investors table.');
+      Investors.seed();
     }
   });
 };
